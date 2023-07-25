3f41bfbebc5cae821e1664fb09da5ff31d4f2ea3
Mostafa Mabrok <mostafa.m.mabrok@gmail.com>
2023-07-25 15:33:42 +0300

    Changed listprograms() to listmathprograms()
    This will allow the creation of different catergories

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from LinearEquation import *
 
 project_name = "Project NAME!"
 
-def listprograms():
+def listmathprograms():
     print("1-Quadratic Equation solving tool")
     print("2-Solve for Slope using 2 points")
     print("3-Solve for X or Y intercept")
@@ -26,6 +26,6 @@ def listprograms():
 def welcome():
     print("Welcome to the " + project_name)
     print("\nPlease Select the program you would like to use by pressing the respective number and pressing enter.\n")
-    listprograms()
+    listmathprograms()
 
 welcome()
