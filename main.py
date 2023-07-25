1c13aaf0c1a72fa7d1f70a85bb8782cc4ffabdd5
Mostafa Mabrok <mostafa.m.mabrok@gmail.com>
2023-07-25 13:47:41 +0300

    added LE solever to main

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 from QuadraticEquation import *
+from LinearEquation import *
 
 project_name = "Project NAME!"
 
 def listprograms():
     print("1-Quadratic Equation solving tool")
-    print("2-PLACEHOLDER")
+    print("2-Linear")
     print("3-PLACEHOLDER\n")
     
     chosenprogram = input("")
     
     if chosenprogram == "1":
         solveQE()
+        welcome()
+    if chosenprogram == "1":
+        slopesolve()
         welcome()
 
 def welcome():
