830596d0157a3a51c885d4e63261386ee2e11247
Mostafa Mabrok <mostafa.m.mabrok@gmail.com>
2023-07-25 14:37:23 +0300

    Added the ability to solve for x and y intercepts.

--- LinearEquation.py
+++ LinearEquation.py
@@ -8,6 +8,32 @@ def slopesolve():
     slope=(y2-y1)/(x2-x1)
     print("\n Your slope is:" + str(slope) + " !")
     
+def solve_x_intercept():
+    y_int_slope = int(input("Slope of the line: "))
+    x= int(input("\nX of the point: "))
+    y= int(input("Y of the point: "))
+    y_int = y-(x*y_int_slope)
+    x_int = y_int_slope*0+y_int
+    print("The x intercept of this line is: " + str(x_int))
+
+def solve_y_intercept():
+    y_int_slope = int(input("Slope of the line: "))
+    x= int(input("\nX of the point: "))
+    y= int(input("Y of the point: "))
+    y_int = y-(x*y_int_slope)
+    print("The y intercept of this line is: " + str(y_int))
+    
+def interceptsolve():
+    xory = input("Would you like to solve for x-intercept or y-intercept (x/y): ")
+    
+    if xory == "x":
+        solve_x_intercept()
+    
+    if xory == "y":
+        solve_y_intercept()
+        
+
+    
    
     
     
