b871ed6a3a009eb635258f115996c90363edceed
Mostafa Mabrok <mostafa.m.mabrok@gmail.com>
2023-07-25 13:46:18 +0300

    Added a new file called LinearEquation and added a slope solve function
    This is a very useful tool to get the slope quickly, this file will be used ot solve other sapecs of linear equations as well

---
+++ LinearEquation.py
@@ -0,0 +1,14 @@
+def slopesolve():
+    print("This program will help you easily obtain the slope of a given line.")
+    print("Type the respetive coordinates in the following prompts.\n")
+    x1= int(input("X of the first point: "))
+    y1= int(input("Y of the first point: "))
+    x2= int(input("X of the second point: "))
+    y2= int(input("Y of the second point: "))
+    slope=(y2-y1)/(x2-x1)
+    print("\n Your slope is:" + str(slope) + " !")
+    
+   
+    
+    
+
