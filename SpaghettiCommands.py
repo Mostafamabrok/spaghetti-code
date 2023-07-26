3428771bdac7b0fbdf2aa2dc615c7f976703cc5d
Mostafa Mabrok <mostafa.m.mabrok@gmail.com>
2023-07-26 22:42:16 +0300

    Added comments for comprehensibility

--- SpaghettiCommands.py
+++ SpaghettiCommands.py
@@ -3,13 +3,14 @@ from QuadraticEquation import *
 from LinearEquation import *
 from FileSorter import * 
  
+#This function starts and runs the CLI and checks the use input for commands. NOTE: Better system needed
 def callcommand():
-     
+    #This is the where the user inputs their commands
     command = input(": ")
-    
+    #These if statements check for commands by checking what word is written at the start. When it finds a command associated with the word it removes the word and goes on to the next word
     if command.startswith("help"):
         print("Help Guide: \n -An inbuilt help guide for the command line interface is TBA, for more info, go to the Github repo page")
-    
+    #This if statement checks for math commands
     if command.startswith("solve"):
         command2 = command.replace("solve", "")
         if command2.startswith(" QE"):
@@ -23,7 +24,7 @@ def callcommand():
         if command2.startswith(" intercept"):
             interceptsolve()
             callcommand()
-            
+    #This if statement checks for file category commands        
     if command.startswith("file"):
         command2 = command.replace("file")
         if command2.startswih(" sort"):
--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from QuadraticEquation import *
 from LinearEquation import *
 from FileSorter import *
 
-
+#self explanatory 
 project_name = "Spaghetti Project"
 
-
+#Runs the CLI from SpaghettiCommands
 def welcome():
     print("Welcome to the " + project_name)
     callcommand()
 
         
-
+#Starts the entire program
 welcome()
