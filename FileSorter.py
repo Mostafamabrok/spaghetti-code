4ca4ce6ccfa4de9dab984950eb69b2c9e78bda54
Mostafa Mabrok <mostafa.m.mabrok@gmail.com>
2023-07-28 13:55:33 +0300

    Added a try and except for conveinece

--- FileSorter.py
+++ FileSorter.py
@@ -1,22 +1,28 @@
 import shutil
 import os
+
+
 def sort_files():
- #This takes the directory the user wants to organize as input
- directory = input("Folder you want to organize, Input the full path please: ")
+ try:
+  #This takes the directory the user wants to organize as input
+  directory = input("Folder you want to organize, Input the full path please: ")
 
- targets = [os.path.join(directory,"pdfs"), os.path.join(directory,"Pictures"), os.path.join(directory,"worddocs"), os.path.join(directory,"zipfiles"), os.path.join(directory,"txtfiles"), os.path.join(directory,"data"),os.path.join(directory,"misc")]
+  if directory == "end":
+    quit()
 
- #This function is just to recude code duplication and it moves <filename> to the target directory using the shutil module 
- def move():
+  targets = [os.path.join(directory,"pdfs"), os.path.join(directory,"Pictures"), os.path.join(directory,"worddocs"), os.path.join(directory,"zipfiles"), os.path.join(directory,"txtfiles"), os.path.join(directory,"data"),os.path.join(directory,"misc")]
+
+  #This function is just to recude code duplication and it moves <filename> to the target directory using the shutil module 
+  def move():
     shutil.move(os.path.join(directory, filename), os.path.join(target, filename))
 
- #The loop that checks that the folders that <filename> will be sorted into exist
- for paths in targets:
+  #The loop that checks that the folders that <filename> will be sorted into exist
+  for paths in targets:
     if not os.path.isdir(paths):
         os.mkdir(paths)
 
- #The actual loop that sorts the files
- for filename in os.listdir(directory):
+  #The actual loop that sorts the files
+  for filename in os.listdir(directory):
 
     if filename.endswith(".pdf"):
         target = targets[0]
@@ -46,4 +52,7 @@ def sort_files():
     if (filename.endswith(".xlsx") or filename.endswith(".csv") or filename.endswith(".json")):
         target = targets[5]
         move()
-        
+ except FileNotFoundError as err:
+   print(err)
+   print("Please Input a valid path.")
+   sort_files()
