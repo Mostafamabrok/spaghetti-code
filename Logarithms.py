087ae4b2203c73d5c5b4e6ba2df511a280a6dac4
Mostafa Mabrok <mostafa.m.mabrok@gmail.com>
2023-07-28 17:40:44 +0300

    chnaged variable "log_base2" to "argument" this improves comprehension

--- Logarithms.py
+++ Logarithms.py
@@ -8,7 +8,7 @@ import numpy as np
 def solveLog():
     print("Welcome to the Logarithm Solver!")
     log_base = int(input("Input the Logarithmic base (the little number next to the 'Log'):"))
-    log_base2 = int(input("Input the number next to the logarithmic base: "))
+    argument = int(input("Input the number next to the logarithmic base: "))
     #Uses math and the previous two inputs to calculate the solution to Log_b(b^x) = x
-    solution = math.log(log_base2, log_base)
-    print("\nLog_"+str(log_base) + "("+ str(log_base2) + ")" + "="  + str(solution))
+    solution = math.log(argument, log_base)
+    print("\nLog_"+str(log_base) + "("+ str(argument) + ")" + "="  + str(solution))
