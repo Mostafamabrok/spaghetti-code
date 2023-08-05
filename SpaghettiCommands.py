7327f04603c152ea6b3353b0d941e1fd67484c26
Mostafa Mabrok <mostafa.m.mabrok@gmail.com>
2023-08-05 11:42:29 +0300

    experimented with solving Issue #1

--- SpaghettiCommands.py
+++ SpaghettiCommands.py
@@ -3,11 +3,22 @@ from QuadraticEquation import *
 from LinearEquation import *
 from FileSorter import * 
 from Logarithms import *
- 
+
+
 #This function starts and runs the command line and checks the use input for commands. NOTE: Better system needed
 def callcommand():
     #This is the where the user inputs their commands
     command = input(": ")
+    
+#    arguments = {
+#        "solve" : [QE, "slope", "intercept"],
+#        "file" : ["sort"]
+#        }
+
+#    for start_arg in arguments.keys():
+#       if command.startswith(start_arg):
+#            pass
+
     #These if statements check for commands by checking what word is written at the start. When it finds a command associated with the word it removes the word and goes on to the next word
     if command.startswith("help"):
         print("Help Guide: \n -An inbuilt help guide for the command line interface is TBA, for more info, go to the Github repo page")
