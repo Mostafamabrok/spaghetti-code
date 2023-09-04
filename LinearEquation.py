b40c7d26de24d261d59f6f1a9863108b3cc8af3b
Mostafa Mabrok <75213491+Mostafamabrok@users.noreply.github.com>
2023-09-04 08:46:06 +0300

    Fixed typo in SlopeSolve string.

--- LinearEquation.py
+++ LinearEquation.py
@@ -1,6 +1,6 @@
 def slopesolve():
     print("This program will help you easily obtain the slope of a given line.")
-    print("Type the respetive coordinates in the following prompts.\n")
+    print("Type the respective coordinates in the following prompts.\n")
     x1= int(input("X of the first point: "))
     y1= int(input("Y of the first point: "))
     x2= int(input("X of the second point: "))
