842c5141727667341a74f7f7774c0072d3c0ec66
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-10-10 19:22:45 +0300

    Compacted spahetti commands Fix #1.

--- SpaghettiCommands.py
+++ SpaghettiCommands.py
@@ -4,33 +4,28 @@ from LinearEquation import *
 from FileSorter import * 
 from Logarithms import *
 
-
-#This function starts and runs the command line and checks the use input for commands. NOTE: Better system needed
+#This function starts and runs the command line and checks the use input for commands.
 def callcommand():
 
     command = input(": ") #This is the where the user inputs their commands  
 
     if command.startswith("help"):
-        print("Help Guide: \n -An inbuilt help guide for the command line interface is TBA, for more info, go to the Github repo page")
-        
-    if command.startswith("solve"): #This if statement checks for math commands
+        print("Help Guide:\n-Inbuilt help TBA, go to Github page for help.\n")
+
+    if command.startswith("solve"): #Checks for math-related commands
 
         command2 = command.replace("solve", "")
+
         if command2.startswith(" QE"): solveQE()
         if command2.startswith(" slope"): slopesolve()            
         if command2.startswith(" intercept"): interceptsolve()
         if command2.startswith(" log"): solveLog()
-
-    #This if statement checks for file category commands        
-    if command.startswith("file"):
+  
+    if command.startswith("file"): #This if statement checks for file-related commands.
         command2 = command.replace("file", "")
-        if command2.startswith(" sort"):
-            sort_files()
+        if command2.startswith(" sort"): sort_files()
             
     if command == "end": quit()
     else: print("Not a valid argument")
         
-    callcommand()
-    
-            
-    
+    callcommand() 
