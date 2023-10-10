383dea4a3a79b4d27cfc61674a382dfd610248df
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-10-10 19:45:39 +0300

    Simple code readability enhancements.

--- QuadraticEquation.py
+++ QuadraticEquation.py
@@ -31,23 +31,27 @@ def solveQE():
  else:
   print("The routes are imaginary, we didnt have the funding to include that.")
 
- #Asking for persmission to plot the equation
+
  plotpermission = input("Do you want to plot your quadratic equation (y/n): ")
 
  if plotpermission == "y" :
   print("Undergoing plotting process...\n")
   print("Plotted!")
+
 #Setting up plotting variables
   x_plot = np.linspace(-10, 10,100)
   y_plot = a*(x_plot**2) + b*x_plot + c
+
   fig = plt.figure()
   ax = fig.add_subplot(1, 1, 1)
+
   ax.spines['left'].set_position('center')
   ax.spines['bottom'].set_position('zero')
   ax.spines['right'].set_color('none')
   ax.spines['top'].set_color('none')
   ax.xaxis.set_ticks_position('bottom')
   ax.yaxis.set_ticks_position('left')
+  
   plt.plot(x_plot, y_plot, 'r')
   plt.show()
   print("Thank you for using the QES!")
